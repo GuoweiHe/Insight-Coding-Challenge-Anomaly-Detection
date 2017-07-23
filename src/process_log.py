e0c97918f72a24690ee8d2dc02aa163f111d3200
Guowei He <guoweirichard@gmail.com>
2017-07-23 02:12:15 -0500

    Update process_log.py

--- src/process_log.py
+++ src/process_log.py
@@ -21,58 +21,58 @@ def initialize(filename):
     for nextline in inputfile:
         if nextline == '\n':
             continue
-        new_log = json.loads(nextline)
+        new_event = json.loads(nextline)
         
         # When a new event is read in, call the function "update_log" to update the dict "history_log".
         index += 1
-        update_log(history_log, new_log, T, index)
+        update_log(history_log, new_event, T, index)
     
     inputfile.close()
     return (history_log, D, T, index)
 
 
 ## This function updates the dict "historty_log" with newly read in event.
-def update_log(history_log, new_log, T, index):
+def update_log(history_log, new_event, T, index):
     # If the event type is "purchase", add index number and purchase amount to the dict.
-    if new_log['event_type'] == 'purchase':
-        ID = new_log['id']
+    if new_event['event_type'] == 'purchase':
+        ID = new_event['id']
         # Check if the dictionary has the ID.
         if not ID in history_log:
             history_log[ID] = {}
-            history_log[ID]['friend'] = []
-            history_log[ID]['index'] = []
             history_log[ID]['amount'] = []
+            history_log[ID]['index'] = []
+            history_log[ID]['friend'] = []
         
         # Add index number and purchase amount for the ID.
+        history_log[ID]['amount'].append(float(new_event['amount']))
         history_log[ID]['index'].append(index)
-        history_log[ID]['amount'].append(float(new_log['amount']))
         
         # To save some disk space, if a user has more than T purchases, we just keep the latest T purchases and delete the older purchases.
         if (len(history_log[ID]['amount']) > T):
-            history_log[ID]['index'].pop(0)
             history_log[ID]['amount'].pop(0)
+            history_log[ID]['index'].pop(0)
     
     # If the event type is "befriend", add the user IDs to each other's friend list.
-    if new_log['event_type'] == 'befriend':
-        ID1 = new_log['id1']
-        ID2 = new_log['id2']
+    if new_event['event_type'] == 'befriend':
+        ID1 = new_event['id1']
+        ID2 = new_event['id2']
         if not ID1 in history_log:
             history_log[ID1] = {}
-            history_log[ID1]['friend'] = []
-            history_log[ID1]['index'] = []
             history_log[ID1]['amount'] = []
+            history_log[ID1]['index'] = []
+            history_log[ID1]['friend'] = []
         if not ID2 in history_log:
             history_log[ID2] = {}
-            history_log[ID2]['friend'] = []
-            history_log[ID2]['index'] = []
             history_log[ID2]['amount'] = []
+            history_log[ID2]['index'] = []
+            history_log[ID2]['friend'] = []
         history_log[ID1]['friend'].append(ID2)
         history_log[ID2]['friend'].append(ID1)
     
     # If the event type is "unfriend", remove the IDs from friend lists.
-    if new_log['event_type'] == 'unfriend':
-        ID1 = new_log['id1']
-        ID2 = new_log['id2']
+    if new_event['event_type'] == 'unfriend':
+        ID1 = new_event['id1']
+        ID2 = new_event['id2']
         if (ID1 in history_log and ID2 in history_log[ID1]['friend']):
             history_log[ID1]['friend'].remove(ID2)
         if (ID2 in history_log and ID1 in history_log[ID2]['friend']):
@@ -86,21 +86,21 @@ def find_anomaly(history_log, index, D, T, inputfile_name, outputfile_name):
     for nextline in infile:
         if nextline == '\n':
             continue
-        new_log = json.loads(nextline)
+        new_event = json.loads(nextline)
         
         # When a new event is read in, call the function "updated_log" to update the dict "history_log".
         index += 1
-        update_log(history_log, new_log, T, index)
+        update_log(history_log, new_event, T, index)
     
-        if new_log['event_type'] == 'purchase':
+        if new_event['event_type'] == 'purchase':
             # Call function "calculate_mean_sd" to calculate the mean and sd of the user's social network.
-            (mean, sd) = calculate_mean_sd(history_log, new_log['id'], D, T)
+            (mean, sd) = calculate_mean_sd(history_log, new_event['id'], D, T)
             
             # mean == -1 and sd == -1 indicate that the user's social network has less than 2 purchases.
             if (mean == -1 and sd == -1):
                 continue
             # If the user's purchase amount is greater than mean + 3 * sd, output the abnormal purchase.
-            if (float(new_log['amount']) > mean + 3 * sd):
+            if (float(new_event['amount']) > mean + 3 * sd):
                 string = '{"event_type":"%s", "timestamp":"%s", "id": "%s", "amount": "%s", "mean": "%.2f", "sd": "%.2f"}\n' % (new_log["event_type"], new_log["timestamp"], new_log["id"], new_log["amount"], mean, sd)
                 outfile.write(string)
 
@@ -110,7 +110,7 @@ def find_anomaly(history_log, index, D, T, inputfile_name, outputfile_name):
 
 
 ## Given a degree D and a customer's ID, this function returns all the friends in the user's Dth degree social network.
-def get_friend(history_log, ID, D):
+def get_friends(history_log, ID, D):
     friends = set()
     current_degree_friend = set();
     current_degree_friend.add(ID)
@@ -132,14 +132,14 @@ def get_friend(history_log, ID, D):
 ## This function calculates the mean and standard deviation of a given user's Dth degree social network.
 def calculate_mean_sd(history_log, ID, D, T):
     # Find the friends in the user's Dth degree social network.
-    friend = get_friend(history_log, ID, D)
-    length = len(friend)
+    friends = get_friends(history_log, ID, D)
+    length = len(friends)
     
     # Since all user's purchase lists have already sorted from old to new, 
     # when we search the latest purchases, we can start from the end of the lists.
     position = []
     for i in range(length):
-        position.append(len(history_log[friend[i]]['index']) - 1)  
+        position.append(len(history_log[friends[i]]['index']) - 1)  
     latest_amount = []
     
     while True:
@@ -147,14 +147,14 @@ def calculate_mean_sd(history_log, ID, D, T):
         latest_list = -1
         # For each friend's index list, find the latest index and the index list with this largest index.
         for i in range(length):
-            if (position[i] >= 0 and history_log[friend[i]]['index'][position[i]] > latest_index):
-                latest_index = history_log[friend[i]]['index'][position[i]]
+            if (position[i] >= 0 and history_log[friends[i]]['index'][position[i]] > latest_index):
+                latest_index = history_log[friends[i]]['index'][position[i]]
                 latest_list = i
         # If there is no more event or we have already get the latest T purchases, exit the while loop
         if (latest_list == -1 or len(latest_amount) == T):
             break;
         # If find the next latest purchase, put the purchase amount in the list 'latest_amout'.
-        latest_amount.append(history_log[friend[latest_list]]['amount'][position[latest_list]])
+        latest_amount.append(history_log[friends[latest_list]]['amount'][position[latest_list]])
         position[latest_list] -= 1
             
     # If there are less than 2 purchases in the social network, return mean = -1 and sd = -1.
